f42e807d670bb26875a94accdd07f14a0c1961f2
Weiliang Li <to.be.impressive@gmail.com>
2023-03-10 10:04:02 +0900

    Add project contract

---
+++ hardhat/contracts/Project.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+// Project contract
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Timers.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Project is Ownable {
+    using Timers for Timers.BlockNumber;
+    using SafeCast for uint256;
+
+    IERC20 public immutable token;
+    string public ID;
+    mapping(address => uint256) public donors;
+
+    Timers.BlockNumber private _deadline;
+
+    constructor(IERC20 _token, string memory _id, uint256 _period) Ownable() {
+        token = _token;
+        ID = _id;
+        _deadline.setDeadline(block.number.toUint64() + _period.toUint64());
+        // NOTE: tx.origin is NOT safe, never use it for authorization
+        _transferOwnership(tx.origin);
+    }
+
+    function isPending() public view returns (bool) {
+        return _deadline.isPending();
+    }
+
+    function isExpired() public view returns (bool) {
+        return _deadline.isExpired();
+    }
+
+    function support(uint256 _amount) public {
+        require(isPending(), "Project: You cannot support after expiration");
+        require(
+            token.transferFrom(_msgSender(), address(this), _amount),
+            "Project: You need to approve first"
+        );
+        donors[_msgSender()] += _amount;
+    }
+
+    function withdraw() public onlyOwner {
+        require(
+            isExpired(),
+            "Project: You cannot withdraw before expiration"
+        );
+
+        token.transfer(owner(), token.balanceOf(address(this)));
+    }
+}
--- hardhat/contracts/T3TimeCoin.sol
+++ hardhat/contracts/T3TimeCoin.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
-// Governance token for voting
+// Time token for project budget
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract T3Token is ERC20, ERC20Permit, ERC20Votes {
+contract T3TimeCoin is ERC20, ERC20Permit, ERC20Votes {
     constructor(
         uint256 _initialSupply
-    ) ERC20("T3Token", "T3T") ERC20Permit("T3Token") {
+    ) ERC20("T3TimeCoin", "TTC") ERC20Permit("T3TimeCoin") {
         _mint(_msgSender(), _initialSupply);
         _delegate(_msgSender(), _msgSender());
     }
 
+    // The functions below are overrides required by Solidity.
     function _afterTokenTransfer(
         address from,
         address to,
@@ -34,9 +35,5 @@ contract T3Token is ERC20, ERC20Permit, ERC20Votes {
         uint256 amount
     ) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
-    }
-
-    function airdrop() public {
-        //TODO
     }
 }
